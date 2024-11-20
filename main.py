12130668e30e6b1800535020f7f2298b88929e61
decobwebs <cobwebb784@gmail.com>
2024-11-19 16:40:35 -1200

    Use env vairables and create Procfile.

--- main.py
+++ main.py
@@ -10,8 +10,20 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
+from dotenv import load_dotenv
 
 app = Flask(__name__)
+
+# Constants
+# Load environment variables from the .env file
+load_dotenv()
+# Configuration using environment variables
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
+MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+ADMIN_EMAIL = os.environ.get('ADMIN_EMAIL')
+SMTP_SERVER = os.environ.get('SMTP_SERVER')
+SMTP_PORT = int(os.environ.get('SMTP_PORT', 587))  # Default to 587 if not found
 
 # Constants
 LOGIN_CSV = "login_data.csv"
@@ -19,12 +31,6 @@ LOGOUT_CSV = "logout_data.csv"
 EXCEL_FILE = "output/SessionDetails.xlsx"
 QR_LOGIN_DIR = "qr_codes/login"
 QR_LOGOUT_DIR = "qr_codes/logout"
-ADMIN_EMAIL = "sadiqabuidris@gamail.com"  # Replace with actual admin email
-SMTP_SERVER = "smtp.gmail.com"
-SMTP_PORT = 587
-MAIL_USERNAME = "team.afssltd@gmail.com"  # Replace with your email
-MAIL_PASSWORD = "lajk rvva ytaa qsxe"  # Replace with your email password
-
 # Ensure required directories exist
 os.makedirs(QR_LOGIN_DIR, exist_ok=True)
 os.makedirs(QR_LOGOUT_DIR, exist_ok=True)
@@ -142,5 +148,6 @@ if __name__ == "__main__":
     # Start inactivity monitoring in a separate thread
     threading.Thread(target=monitor_inactivity, daemon=True).start()
 
-    # Run Flask app
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    # Run Flask app in production mode
+    app.run(host="0.0.0.0", port=5000, debug=False)
+
